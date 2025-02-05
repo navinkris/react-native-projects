b4a65f5722f0ac185b290bbe0564ecb773a1000e
navkrish <navin.krishnamurthi@nokia.com>
2025-02-05 12:37:19 +0530

    **Jira:** NSDWAN-BluetoothScanner
    **Description:**
    - Added code related to react-native-ble-plx
    
    **Testing:**
    - Verified the changes in the emulator and H/W, works fine
    
    **Notes:**
    - N/A

---
+++ BluetoothScannerApp/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BluetoothScanner from './screens/BluetoothScanner';
+
+function App(): React.JSX.Element {
+  return <BluetoothScanner />;
+}
+
+export default App;
